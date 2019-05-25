4be5338b588349fdaee71bb86bc2f630d44dabe2
cerelac_rox <cerelac_rox@hotmail.com>
2019-05-25 16:41:04 -0500

    Added support for new API and use of requests

--- lifxscreen.py
+++ lifxscreen.py
@@ -1,22 +1,24 @@
-# Author: Frak Al-Nuaimy 
-# email: frakman@hotmail.com
-
 from PIL import ImageGrab
 import time
 import os
 from colour import Color
+import requests
 
 #//////////////////////////////////////////////////////////////////////////////////////////////////////////
 # GLOBAL DEFINES
 #//////////////////////////////////////////////////////////////////////////////////////////////////////////
 #HEIGHT         = 1920   #now using image.size[1] dynamically
 #WIDTH          = 1200   #now using image.size[0] dynamically
-LOOP_INTERVAL  = 3    # how often we calculate screen colour (in seconds)
-DURATION       = 3    # how long it takes bulb to switch colours (in seconds)
+LOOP_INTERVAL  = 1    # how often we calculate screen colour (in seconds)
+DURATION       = 1    # how long it takes bulb to switch colours (in seconds)
 DECIMATE       = 10   # skip every DECIMATE number of pixels to speed up calculation
 #get your unit-unique token from http://developer.lifx.com/ and use it here
-TOKEN          = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" 
+TOKEN          = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" 
 BULB_NAME      = "all"  # you can use any label you've assigned your bulb here, but if you do, make sure you use the curl syntax alternative with "label:" instead. I provided a commented out version of it below.
+headers = {
+    "Authorization": "Bearer %s" % TOKEN,
+}
+
 #//////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -50,19 +52,14 @@ while True:
 	green = ((green / ( (image.size[1]/DECIMATE) * (image.size[0]/DECIMATE) ) ) )/255.0
 	blue = ((blue / ( (image.size[1]/DECIMATE) * (image.size[0]/DECIMATE) ) ) )/255.0
 	c= Color(rgb=(red, green, blue))  
-	print c
+	print(c)
 	
-	print "\n average   red:%s green:%s blue:%s" % (red,green,blue)
+	print("\n average   red:%s green:%s blue:%s" % (red,green,blue))
 	#print "\n average   hue:%f saturation:%f luminance:%f" % (c.hue,c.saturation,c.luminance)
-	print "\n average  (hex) "+  (c.hex)
-	#//////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-	#//////////////////////////////////////////////////////////////////////////////////////////////////////////
-	# PROGRAM LIFX BULB WITH COLOUR 
-	#//////////////////////////////////////////////////////////////////////////////////////////////////////////
-	cmd = " c:\\curl\\curl.exe -u \""+TOKEN+":\" -X PUT -d \"color=" + str(c.hex) + "\" -d \"duration=" +str(DURATION)+ "\" \"https://api.lifx.com/v1beta1/lights/"+BULB_NAME+"/color\"" 
-	#cmd = " c:\\curl\\curl.exe -u \""+TOKEN+":\" -X PUT -d \"color=" + str(c.hex) + "\" -d \"duration=" +str(DURATION)+ "\" \"https://api.lifx.com/v1beta1/lights/label:"+BULB_NAME+"/color\"" 
-	print cmd
-	os.system(cmd)
-	
+	print ("\n average  (hex) "+  (c.hex))
+	payload = {
+    	"color": c.hex,
+    	"fast": True
+	}
+	response = requests.put('https://api.lifx.com/v1/lights/all/state', data=payload, headers=headers)
 
